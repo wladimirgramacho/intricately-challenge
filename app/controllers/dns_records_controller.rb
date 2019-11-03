9cdb023857facd4a89324c044e4e9d5c2d5747f0
Wladimir Gramacho <wladimirgramacho@gmail.com>
2019-11-03 08:00:23 -0300

    feat: expect controller to call CreateDnsRecord service

--- app/controllers/dns_records_controller.rb
+++ app/controllers/dns_records_controller.rb
@@ -1,5 +1,14 @@
 class DnsRecordsController < ApplicationController
   def create
-    
+    ip = create_params[:ip]
+    addresses = create_params[:hostnames_attributes].map {|h| h[:hostname]}
+
+    CreateDnsRecord.new(ip: ip, hostnames: addresses).process
+  end
+
+  private
+
+  def create_params
+    params.require(:dns_record).permit(:ip, hostnames_attributes: [:hostname]).to_h
   end
 end 
--- spec/controllers/dns_records_controller_spec.rb
+++ spec/controllers/dns_records_controller_spec.rb
@@ -4,7 +4,7 @@ describe DnsRecordsController, type: :controller do
   describe 'POST #create' do
     let(:params) {
       {
-        "dns_records": {
+        "dns_record": {
           "ip": "1.1.1.1",
           "hostnames_attributes": [
             {
@@ -12,12 +12,17 @@ describe DnsRecordsController, type: :controller do
             }
           ]
         }
-      }
+      }.as_json
     }
 
     it 'returns success with valid params' do
       post :create, params: params
       expect(response).to have_http_status(:success)
+    end
+
+    it 'calls CreateDnsRecord service with params' do
+      expect_any_instance_of(CreateDnsRecord).to receive(:process)
+      post :create, params: params
     end
   end
 end
